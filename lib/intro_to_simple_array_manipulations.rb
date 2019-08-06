1a5e62c2c6caf16a58013e1f3feb4706a06fd967
Emily G <aspentree79@gmail.com>
2019-08-06 13:03:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,19 +1,24 @@
 def using_push(array, string)
 array.push(string)
 end
+# adds string to the end of the array
 
 def using_unshift(array, string)
   array.unshift(string)
 end
+# adds string to beginning of array
 
 def using_pop(array)
   array.pop
 end
+# removes array's last element and returns that element
 
 def pop_with_args(array)
   array.pop(2)
 end
+# removes array's last 2 elements and returns them
 
 def using_shift(array)
   array.shift
 end
+# removes first item of the array and returns it
